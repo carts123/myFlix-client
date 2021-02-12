715b64176c9991f629ab5174d70b4a5ddac95092
carts123 <72449546+carts123@users.noreply.github.com>
2021-02-12 11:19:01 +0000

    Back button added

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -33,6 +33,9 @@ export class MovieView extends React.Component {
           <span className="label">Director: </span>
           <span className="value">{movie.Director.Name}</span>
         </div>
+        <button
+          onClick={this.refreshPage}>GO Back
+        </button>
       </div>
 
 
